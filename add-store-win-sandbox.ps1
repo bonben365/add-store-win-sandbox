f58609adb2279dc5c1753ecaf291a7f57ebe2bc2
bonben365 <126295084+bonben365@users.noreply.github.com>
2023-02-25 18:25:30 +0700

    Update add-store-win-sandbox.ps1

--- add-store-win-sandbox.ps1
+++ add-store-win-sandbox.ps1
@@ -18,7 +18,6 @@ Set-Location $env:temp\kms
 
 # Download required files
 $uri = "https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/bonben365.com/Zip/microsoftstore-win-ltsc.zip"
-
 $null = Invoke-WebRequest -Uri $uri -OutFile "microsoftstore-win-ltsc.zip" -ErrorAction:SilentlyContinue
 
 # Extract downloaded file then run the script
@@ -39,9 +38,10 @@ if (!(Get-ChildItem "*WindowsStore*")) {
 }
 
 if ($arch = "x86") {
-    $depens = Get-ChildItem | Where-Object { ($_.Name -match '^*Microsoft.NET.Native*|^*VCLibs*') -and ($_.Name -like '*x86*')}
-} else {
-    $depens = Get-ChildItem | Where-Object { ($_.Name -match '^*Microsoft.NET.Native*|^*VCLibs*') -and ($_.Name -like '*x64*')}
+    $depens = Get-ChildItem | Where-Object {($_.Name -match '^*Microsoft.NET.Native*|^*VCLibs*') -and ($_.Name -like '*x86*')}
+} 
+if ($arch = "x64") {
+    $depens = Get-ChildItem | Where-Object {$_.Name -match '^*Microsoft.NET.Native*|^*VCLibs*'}
 }
 
 Write-Host
