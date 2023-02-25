1bd2234fae89f15bfea572ec45293d5af7d355cf
bonben365 <126295084+bonben365@users.noreply.github.com>
2023-02-25 14:06:51 +0700

    Update add-store-win-sandbox.ps1

--- add-store-win-sandbox.ps1
+++ add-store-win-sandbox.ps1
@@ -12,6 +12,18 @@ if ([System.Environment]::OSVersion.Version.Build -lt 16299) {
     exit
 }
 
+# Create temporary directory
+$null = New-Item -Path $env:temp\kms -ItemType Directory -Force
+Set-Location $env:temp\kms
+
+# Download required files
+$uri = "https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/bonben365.com/Zip/microsoftstore-win-ltsc.zip"
+$null = Invoke-WebRequest -Uri $uri -OutFile "microsoftstore-win-ltsc.zip" -ErrorAction:SilentlyContinue
+
+# Extract downloaded file then run the script
+Expand-Archive .\microsoftstore-win-ltsc.zip -Force -ErrorAction:SilentlyContinue
+Set-Location "$env:temp\kms\microsoftstore-win-ltsc"
+
 if ([System.Environment]::Is64BitOperatingSystem -like "True") {
     $arch = "x64"
 }
